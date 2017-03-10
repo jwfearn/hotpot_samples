024b5fc94fff90332440e9fab7f6bc2ae53eb374
jfearnside <jfearnside@avvo.com>
2017-03-10 09:16:41 -0800

    Skyler's stuff from meetup

--- hotpot_sample/lib/foo_server.ex
+++ hotpot_sample/lib/foo_server.ex
@@ -2,7 +2,20 @@ defmodule FooServer do
   use GenServer
 
   def start_link do
-    GenServer.start_link(__MODULE__, 0, name: __MODULE__)
+    GenServer.start_link(__MODULE__, get_state, name: __MODULE__)
+  end
+
+  defp get_state do
+    case state_alive? do
+      true -> DataStore.get_state
+      false -> 0
+    end
+  end
+
+  defp state_alive? do
+    Enum.any?(Process.registered, fn(item) ->
+      item == DataStore
+    end)
   end
 
   def increment do
@@ -14,10 +27,44 @@ defmodule FooServer do
   end
 
   def handle_cast(:increment, state) do
-    {:noreply, state + 10}
+    {:noreply, state + 100}
   end
 
   def handle_call(:get, _from, state) do
     {:reply, state, state}
   end
+
+  def handle_info(data, state) do
+    IO.inspect data
+    {:noreply, state}
+  end
+
+  def die do
+    GenServer.call(__MODULE__, :die)
+  end
+
+  def handle_call(:die, _, _) do
+    :ok = :not_ok
+  end
+
+  def terminate(reason, state) do
+    IO.inspect reason
+    DataStore.start(state)
+  end
+end
+
+defmodule DataStore do
+  use GenServer
+
+  def start(old_state) do
+    GenServer.start(__MODULE__, old_state, name: __MODULE__)
+  end
+
+  def get_state do
+    GenServer.call(__MODULE__, :get)
+  end
+
+  def handle_call(:get, _from, state) do
+    {:reply, state, state}
+  end 
 end
